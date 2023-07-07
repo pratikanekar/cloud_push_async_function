7a3fe3aa0bd454e471ba03640b354c3ee7d58d4c
pratikanekar <pratikpravinanekar@gmail.com>
2023-07-07 15:43:45 +0530

    # added try except and logging in this code and create formatting

--- cloud_push_async_fucntion.py
+++ cloud_push_async_fucntion.py
@@ -1,31 +1,34 @@
-import asyncio
-import aiohttp
 import time
+import asyncio
+import httpx
+from loguru import logger
+from json import dumps
 from json_maker import gen_data
 
 
-async def send_jsons(jsons):
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for json_data in jsons:
-            task = asyncio.ensure_future(send_json(session, json_data))
-            tasks.append(task)
-
-        await asyncio.gather(*tasks)
-
-
-async def send_json(session, json_data):
-    url = "http://0.0.0.0:5111/test_data/data"
-    headers = {'Content-Type': 'application/json'}
-    async with session.post(url, json=json_data, headers=headers) as response:
-        response_text = await response.text()
-        print(f"Response: {response_text}")
+async def send_data_async(data):
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.post('http://192.168.1.219:5111/test_data/data', data=data)
+            if response.status_code == 200:
+                logger.info(f"Response: {response.text}")
+                return response
+    except Exception as e:
+        logger.error(f"{e}")
 
 
-main_data = gen_data()
-start_time = time.time()
-loop = asyncio.get_event_loop()
-loop.run_until_complete(send_jsons(main_data))
-end_time = time.time()
-execution_time = end_time - start_time
-print(f"Execution time : {execution_time} sec")
+async def main():
+    await send_data_async(final_json_data)
+
+
+try:
+    start_time = time.time()
+    main_data = gen_data()
+    final_json_data = dumps(main_data)
+    # Run the asynchronous event loop
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
+    logger.debug(f"Cloud Push Process Time : {time.time() - start_time}")
+except Exception as e:
+    logger.error(f"{e}")
--- json_maker.py
+++ json_maker.py
@@ -1,49 +1,47 @@
 from datetime import datetime
 import time
+from loguru import logger
 
 
 def gen_data():
-    main_data = []
-    panels = 100
-    measurements = 10
-    device_codes = 20
-    zones = 40
-    count = 0
-    start_time = time.time()
+    try:
+        main_data = []
+        panels = 100
+        measurements = 10
+        device_codes = 20
+        zones = 40
+        count = 0
+        start_time = time.time()
 
-    for panel in range(0, panels):
-        # print(f"[PANEL]: {panel}")
-        for meas in range(0, measurements):
-            mm = f"MEASUREMENT_{meas}"
-            # print(f"{mm}")
-            for dc in range(0, device_codes):
-                device_code = f"DEV_{dc}"
-                # print(f"{device_code}")
-                for zn in range(0, zones):
-                    zone = f"ZON_{zn}"
-                    # print(f"{zone}")
-
-                    data_dict = {
-                        "measurement": mm,
-                        "tags": {
-                            "panel_no": panel,
-                            "device_code": device_code,
-                            "zone": zone,
-                            "min_alarm": 0.0,
-                            "max_alarm": 0.0,
-                            "name": "Down Direction Selected",
-                            "alarm_name": "null",
-                            "alarm_state": "null",
-                            "unique_tag": f"{device_code}-{zone}"
-                        },
-                        "fields": {
-                            "value": 0
-                        },
-                        "time": datetime.now().replace(microsecond=0).astimezone().isoformat()
-                    }
-                    count += 1
-                    print(data_dict)
-                    main_data.append(data_dict)
-    print(f"[PROCESS END TIME]: {time.time() - start_time}")
-    print(f"{count}")
-    return main_data
+        for panel in range(0, panels):
+            for meas in range(0, measurements):
+                mm = f"MEASUREMENT_{meas}"
+                for dc in range(0, device_codes):
+                    device_code = f"DEV_{dc}"
+                    for zn in range(0, zones):
+                        zone = f"ZON_{zn}"
+                        data_dict = {
+                            "measurement": mm,
+                            "tags": {
+                                "panel_no": panel,
+                                "device_code": device_code,
+                                "zone": zone,
+                                "min_alarm": 0.0,
+                                "max_alarm": 0.0,
+                                "name": "Down Direction Selected",
+                                "alarm_name": "null",
+                                "alarm_state": "null",
+                                "unique_tag": f"{device_code}-{zone}"
+                            },
+                            "fields": {
+                                "value": 0
+                            },
+                            "time": datetime.now().replace(microsecond=0).astimezone().isoformat()
+                        }
+                        count += 1
+                        main_data.append(data_dict)
+        logger.debug(f"[JSON Making Process]: {time.time() - start_time}")
+        logger.debug(f"{count}")
+        return main_data
+    except Exception as e:
+        logger.info(f"{e}")
