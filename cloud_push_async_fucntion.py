7511ec8115983356319cd74f14db0b9ac1142f3f
pratikanekar <pratikpravinanekar@gmail.com>
2023-07-13 10:24:27 +0530

    # added code of create bulk list and send it simultenously on cloud

--- cloud_push_async_fucntion.py
+++ cloud_push_async_fucntion.py
@@ -3,33 +3,41 @@ import asyncio
 import httpx
 from loguru import logger
 from json import dumps
+from fastapi import HTTPException
 from json_maker import gen_data
 
 
 async def send_data_async(data):
     try:
         async with httpx.AsyncClient() as client:
-            response = await client.post('http://192.168.1.219:5111/test_data/data', data=data)
+            response = await client.post('http://192.168.1.236:5111/test_data/data', data=data, timeout=250)
             if response.status_code == 200:
                 logger.info(f"Response: {response.text}")
                 return response
+            else:
+                logger.critical(f"Response: {response.text}")
     except Exception as e:
         logger.error(f"{e}")
 
 
-async def main():
-    await send_data_async(final_json_data)
+async def main(final_json_data):
+    try:
+        await send_data_async(final_json_data)
+    except Exception as e:
+        logger.error(f"{e}")
+
 
 
 try:
     logger.info(f"Started Creation of json files.....")
     start_time = time.time()
     main_data = gen_data()
-    final_json_data = dumps(main_data)
+    # final_json_data = dumps(multiple_lists)
     # Run the asynchronous event loop
     logger.info(f"Started Cloud Pushing......")
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    for lists in range(0, 1):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main(dumps(main_data)))
 
     logger.debug(f"[Cloud Push Process Time] : {time.time() - start_time}")
 except Exception as e:
--- json_maker.py
+++ json_maker.py
@@ -9,27 +9,29 @@ def gen_data():
         panels = 100
         measurements = 10
         device_codes = 20
-        zones = 40
-        count = 0
-        start_time = time.time()
+        zones = 60
 
         for panel in range(0, panels):
+            count = 0
+            start_time = time.time()
+            pan = f"PANEL_{panel}"
             for meas in range(0, measurements):
                 mm = f"MEASUREMENT_{meas}"
                 for dc in range(0, device_codes):
                     device_code = f"DEV_{dc}"
                     for zn in range(0, zones):
                         zone = f"ZON_{zn}"
-                        data_dict = {
+                        data_dict = [{
                             "measurement": mm,
                             "tags": {
-                                "panel_no": panel,
+                                "panel_no": pan,
                                 "device_code": device_code,
                                 "zone": zone,
                                 "min_alarm": 0.0,
                                 "max_alarm": 0.0,
                                 "name": "Down Direction Selected",
                                 "alarm_name": "null",
+                                "uuid_tag": f"{panel}_{device_code}_{zone}",
                                 "alarm_state": "null",
                                 "unique_tag": f"{device_code}-{zone}"
                             },
@@ -37,11 +39,13 @@ def gen_data():
                                 "value": 0
                             },
                             "time": datetime.now().replace(microsecond=0).astimezone().isoformat()
-                        }
+                        }]
                         count += 1
                         main_data.append(data_dict)
-        logger.debug(f"[JSON Making Process Time]: {time.time() - start_time}")
-        logger.debug(f"{count}")
+            logger.debug(f"[JSON Making Process Time]: {time.time() - start_time}")
+            logger.debug(f"{count}")
+        # print(main_data)
         return main_data
     except Exception as e:
         logger.info(f"{e}")
+# gen_data()
